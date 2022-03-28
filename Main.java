7e3d107a53583e05a07460ffa159789952dedd4b
Lostmaniac9 <jdog2349@gmail.com>
2022-03-28 17:15:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.locks.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+// make a list of 500k numbers and randomly shuffle it and then start pulling from it.
+// 
 
