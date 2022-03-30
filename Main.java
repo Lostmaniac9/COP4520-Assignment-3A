5d5cf469e9d0dfaff3817e8c491b977d029d5259
Lostmaniac9 <jdog2349@gmail.com>
2022-03-30 13:29:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,3 +6,107 @@ import java.util.concurrent.ThreadLocalRandom;
 // make a list of 500k numbers and randomly shuffle it and then start pulling from it.
 // 
 
+private class Node
+{
+  T item;
+  int key;
+  AtomicMarkableReference<Node> next;
+  boolean marked = false;
+}
+
+class Window
+{
+  public Node pred, curr;
+  
+  Window(Node myPreed, Node myCurr)
+  {
+    pred = myPred;
+    curr = myCurr;
+  }
+  
+  Window find(Node head, int key)
+  {
+    Node pred = null, curr = null, succ = null;
+    booleanp[] marked = false;
+    boolean snip;
+    
+    retry: while(true)
+    {
+      pred = head;
+      curr = pred.next.getReference();
+      while(true)
+      {
+        succ = curr.next.get(marked);
+        while(marked[0])
+        {
+          snip = pred.next.compareAndSet(curr, succ, false, false);
+          if(!snip) continue retry;
+          curr = succ;
+          succ = curr.next.get(marked);
+        }
+        if(curr.key >= key)
+          return new Window(pred, curr);
+        pred = curr;
+        curr = succ;
+      }
+    }
+  }
+}
+
+class LockFreeList
+{
+  public boolean add(T item)
+  {
+    int key = item.hashCode();
+    while(true)
+    {
+      Window window = find(head, key);
+      Node pred = window.pred, curr = window.curr;
+      if(curr.key == key)
+      {
+        return false;
+      } else
+      {
+        Node node = new Node(item);
+        node.next = new AtomicMarkableReference(curr, false);
+        if(pred.next.compareAndSet(curr, node, false, false))
+          return true;
+      }
+    }
+  }
+  
+  public boolean remove(T item)
+  {
+    int key = item.hashCode();
+    boolean snip;
+    while(true)
+    {
+      Window window = find(head, key);
+      Node pred = window.pred, curr = window.curr;
+      if(curr.key != key)
+      {
+        return false;
+      } else
+      {
+        Node succ = curr.next.getReference();
+        snip = curr.next.compareAndSet(succ, succ, false, true);
+        if(!snip)
+          continue;
+        pred.next.compareAndSet(curr, succ, false false);
+        return true;
+      }
+    }
+  }
+  
+  public boolean contains(T item)
+  {
+    int key = item.hashCode();
+    Node curr = head;
+    while(curr.key < key)
+    {
+      curr = curr.next.getReference();
+    }
+    return (curr.key == key && !curr.next.isMarked());
+  }
+}
+
